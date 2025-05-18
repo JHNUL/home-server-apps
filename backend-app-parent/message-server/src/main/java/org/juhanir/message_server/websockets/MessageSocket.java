5d635f319888c01e7b503e9896830b20be9740fd
Juhani Riisiö <juhani.riisio@gmail.com>
2025-05-18 12:27:53 +0300

    add basic websocket class and configure logging

---
+++ backend-app-parent/message-server/src/main/java/org/juhanir/message_server/websockets/MessageSocket.java
@@ -0,0 +1,32 @@
+package org.juhanir.message_server.websockets;
+
+import io.quarkus.websockets.next.OnClose;
+import io.quarkus.websockets.next.OnOpen;
+import io.quarkus.websockets.next.WebSocket;
+import io.quarkus.websockets.next.WebSocketConnection;
+import jakarta.inject.Inject;
+import org.jboss.logging.Logger;
+
+@WebSocket(path = "/messages/{user}")
+public class MessageSocket {
+
+    private static final Logger LOG = Logger.getLogger(MessageSocket.class);
+    private final WebSocketConnection connection;
+
+    @Inject
+    public MessageSocket(WebSocketConnection connection) {
+        this.connection = connection;
+    }
+
+    @OnOpen
+    public void onOpen() {
+        String user = connection.pathParam("user");
+        LOG.info("User %s joined".formatted(user));
+    }
+
+    @OnClose
+    public void onClose() {
+        String user = connection.pathParam("user");
+        LOG.info("User %s left".formatted(user));
+    }
+}
