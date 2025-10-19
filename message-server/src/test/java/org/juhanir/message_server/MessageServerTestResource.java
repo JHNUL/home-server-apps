9973249ecf7c972d69934fd9818a420be723443d
Juhani Riisiö <juhani.riisio@gmail.com>
2025-10-19 20:38:47 +0300

    Enable testing

--- message-server/src/test/java/org/juhanir/message_server/MessageServerTestResource.java
+++ message-server/src/test/java/org/juhanir/message_server/MessageServerTestResource.java
@@ -18,21 +18,26 @@ public class MessageServerTestResource implements QuarkusTestResourceLifecycleMa
 
     private static ComposeContainer containers;
 
-    private static final Duration timeout = Duration.of(30, ChronoUnit.SECONDS);
+    private static final Duration WAIT_TIME = Duration.of(30, ChronoUnit.SECONDS);
     private static final String MQTT_CONTAINER = "mqtt_broker";
     private static final String LIQUIBASE_CONTAINER = "liquibase";
+    private static final String KEYCLOAK_CONTAINER = "keycloak";
 
     @Override
     public Map<String, String> start() {
         var mosquittoWaitStrategy = Wait
                 .forLogMessage(".*mosquitto version [0-9]{0,1}\\.[0-9]{0,1}\\.[0-9]{0,3} running.*\\n", 1)
-                .withStartupTimeout(timeout);
+                .withStartupTimeout(WAIT_TIME);
         var liquibaseWaitStrategy = Wait
                 .forLogMessage(".*Liquibase command 'update' was executed successfully.*\\n", 1)
-                .withStartupTimeout(timeout);
+                .withStartupTimeout(WAIT_TIME);
+        var keycloakWaitStrategy = Wait
+                .forLogMessage(".*Realm 'homeserver' imported.*\\n", 1)
+                .withStartupTimeout(WAIT_TIME);
         containers = new ComposeContainer(MessageServerTestResource.getComposeFile())
                 .waitingFor(MQTT_CONTAINER, mosquittoWaitStrategy)
                 .waitingFor(LIQUIBASE_CONTAINER, liquibaseWaitStrategy)
+                .waitingFor(KEYCLOAK_CONTAINER, keycloakWaitStrategy)
                 .withBuild(false);
 
         containers.start();
